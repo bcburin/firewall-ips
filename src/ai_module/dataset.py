1068a33fdcb7d01fce195acdb9a56fb02c8b3d2a
caruso44 <91800368+caruso44@users.noreply.github.com>
2024-04-21 22:42:15 -0300

    Fix nat translation function

--- src/ai_module/dataset.py
+++ src/ai_module/dataset.py
@@ -3,7 +3,7 @@ import math
 import pandas as pd
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
 from graph import create_graph, get_common_neighbors, get_neigbors, get_union_neigbors
 
@@ -34,12 +34,8 @@ def add_metrics(df : pd.DataFrame, col1: int, col2: int) -> pd.DataFrame:
     return df.drop([col1, col2], axis = 1)
 
 
-def check_nat_translation(row : pd.Series,  col1: str, col2: str, col3 :str, col4 :str) -> bool:
-    return (row[col1] == row[col2] or row[col3] == row[col4])
-
-
 def normalize_data(df : pd.DataFrame) -> pd.DataFrame:
-    scaler = MinMaxScaler()
+    scaler = StandardScaler()
     label = df['label']
     df = df.drop('label', axis=1) 
     df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
@@ -55,7 +51,7 @@ def split_data(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series,
 
 def prepare_data(data : pd.DataFrame) -> pd.DataFrame:
     df = prepare_label(data)
-    df['NAT translation'] = df.apply(check_nat_translation, args=('Source Port', 'NAT Source Port', 'Destination Port', 'NAT Destination Port'), axis=1)
+    df['NAT translation'] = df.apply(lambda row: row['Source Port'] == row['NAT Source Port'] or row['Destination Port'] == row['NAT Destination Port'], axis=1)
     df = add_metrics(df, 'Source Port', 'Destination Port')
     df = add_metrics(df, 'NAT Source Port', 'NAT Destination Port')
     df = normalize_data(df)
