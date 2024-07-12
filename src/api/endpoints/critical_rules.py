6e3ff05381ce231801cf401fd16b0ddee160c008
Bruno Burin <cburin.bruno@gmail.com>
2024-07-12 02:22:34 -0300

    [back] fix session injection

--- src/api/endpoints/critical_rules.py
+++ src/api/endpoints/critical_rules.py
@@ -13,14 +13,15 @@ ENTITY = 'critical rule'
 
 @router.get('/', response_model=list[CriticalRuleOutModel])
 def get_all(
+        session: InjectedSession,
         skip: int = Query(default=0, ge=0),
         limit: int | None = Query(default=100, ge=0),
-        session: Session = InjectedSession):
+        ):
     return session.query(CriticalRule).offset(skip).limit(limit).all()
 
 
 @router.get('/{id}', response_model=CriticalRuleOutModel)
-def get_one(id: int, session: Session = InjectedSession):
+def get_one(id: int, session: InjectedSession):
     cr = session.query.get(id)
     if cr is None:
         raise NotFoundDbException(ENTITY)
@@ -28,12 +29,12 @@ def get_one(id: int, session: Session = InjectedSession):
 
 
 @router.post('/', response_model=CriticalRuleOutModel)
-def create(item: CriticalRuleCreateModel, session: Session = InjectedSession):
+def create(item: CriticalRuleCreateModel, session: InjectedSession):
     return CriticalRule.create_from(create_model=item).save(session)
 
 
 @router.put('/{id}', response_model=CriticalRuleOutModel)
-def update(id: int, item: CriticalRuleUpdateModel, session: Session = InjectedSession):
+def update(id: int, item: CriticalRuleUpdateModel, session: InjectedSession):
     if not item.has_updates():
         raise NoUpdatesProvidedDbException(ENTITY)
     cr: CriticalRule = session.query(CriticalRule).get(id)
@@ -43,7 +44,7 @@ def update(id: int, item: CriticalRuleUpdateModel, session: Session = InjectedSe
 
 
 @router.delete('/{id}', response_model=bool)
-def delete_one(id: int, session: Session = InjectedSession):
+def delete_one(id: int, session: InjectedSession):
     user = session.query.get(id)
     if user is None:
         raise NotFoundDbException(ENTITY)
@@ -52,7 +53,7 @@ def delete_one(id: int, session: Session = InjectedSession):
 
 
 @router.delete('/', response_model=bool)
-def delete_multiple(ids: list[int], session: Session = InjectedSession):
+def delete_multiple(ids: list[int], session: InjectedSession):
     session.execute(CriticalRule.__table__.delete().where(CriticalRule.id.in_(ids)))
     session.commit()
     return True
--- src/api/endpoints/firewall_rules.py
+++ src/api/endpoints/firewall_rules.py
@@ -3,8 +3,7 @@ from sqlmodel import Session
 
 from src.common.exceptions.db import NotFoundDbException
 from src.models.firewall_rule import FirewallRule, FirewallRuleOutModel
-from src.services.database import get_session
-
+from src.services.database import get_session, InjectedSession
 
 router = APIRouter(prefix='/firewallRules', tags=['Firewall Rules'])
 
@@ -13,14 +12,15 @@ ENTITY = 'firewall rule'
 
 @router.get('/', response_model=list[FirewallRuleOutModel])
 def get_all(
+        session: InjectedSession,
         skip: int = Query(default=0, ge=0),
         limit: int | None = Query(default=100, ge=0),
-        session: Session = Depends(get_session)):
+        ):
     return session.query(FirewallRule).offset(skip).limit(limit).all()
 
 
 @router.get('/{id}', response_model=FirewallRuleOutModel)
-def get_by_id(id: int, session: Session = Depends(get_session)):
+def get_by_id(id: int, session: InjectedSession):
     fwr = session.query.get(id)
     if fwr is None:
         raise NotFoundDbException(ENTITY)
