9c965af57732525044c248b9ef53bd48c6f5a300
Bruno Burin <cburin.bruno@gmail.com>
2024-08-31 18:00:59 -0300

    [back] create cleaner VersionedObjectManager to deal with persistable object repositories

--- src/services/persistence.py
+++ src/services/persistence.py
@@ -1,151 +1,150 @@
-import logging
-import re
 from datetime import datetime
 from pathlib import Path
 from threading import Lock
-from typing import Type, Generic
+from typing import Generic, Iterable, Type
 
-from src.common.config import CompressionToolOption, PersistenceConfig
+from src.common.config import CompressionToolOption, PersistenceConfig, ConfigurationManager
 from src.common.persistence import PersistableObject, PersistableObjectType
-from src.common.utils import Singleton
+from src.common.utils import LoadableSingleton
 
 
-class PersistableObjectLifeCycleManager(Generic[PersistableObjectType], metaclass=Singleton):
-    _dt_format = "%Y-%m-%d_%H-%M-%S"
+class VersionedObjectManager(Generic[PersistableObjectType], LoadableSingleton):
+    _dt_format = "%Y-%m-%dT%H-%M-%S"
 
-    def __init__(self,
-                 persistable_object_cls: Type[PersistableObject] = None,
-                 persistence_config: PersistenceConfig = None):
+    def __init__(self, cls: Type[PersistableObject]):
+        self._cls = cls
+        self._suffix = cls.generate_hyphen_separated_name() + '.bytes'
+        self._config: PersistenceConfig | None = None
+        self._obj: PersistableObject | None = None
         self._obj_lock = Lock()
-        self._persisted_files_lock = Lock()
-        self._obj = None
-        self._cls = persistable_object_cls
-        self._config = persistence_config
+        self._repository_lock = Lock()
+        super().__init__()
 
-    def object_is_loaded(self) -> bool:
+    def _load(self):
+        self._config = ConfigurationManager().get_server_config().ai_module.persistence
+        if self._config is None:
+            return
+        if not self.empty_repository and self._config.load_latest_on_start:
+            with self._obj_lock:
+                self._obj = self._read_savefile()
+
+    def _loaded(self):
         with self._obj_lock:
-            return self._obj is not None
+            return self._config is not None and (
+                    self._obj is not None or self.empty_repository or not self._config.load_latest_on_start)
 
-    def has_persisted_objects(self) -> bool:
-        return len(self.list_sorted_persisted_filepaths()) != 0
+    def load_latest_version(self):
+        with self.load_guard(), self._obj_lock:
+            self._obj = self._read_savefile()
 
-    def get_object(self) -> PersistableObjectType:
+    def load_version(self, dt: datetime | None):
+        if dt is None:
+            self.load_latest_version()
+        with self.load_guard(), self._obj_lock:
+            self._obj = self._read_savefile(dt)
+
+    def load_new_version(self, obj: PersistableObject):
+        if not isinstance(obj, self._cls):
+            raise Exception()
+        with self._obj_lock:
+            self._obj = obj
+        with self.load_guard(), self._repository_lock:
+            # remove oldest savefiles if max is reached
+            savefile_paths = self._get_savefiles_dict()
+            while len(savefile_paths) >= self._config.max_saved_models:
+                dt = min(savefile_paths.keys())
+                savefile_paths[dt].unlink()
+                del savefile_paths[dt]
+            # write new savefile
+            self._write_savefile(obj)
+
+    def get_loaded_version(self):
         with self._obj_lock:
             return self._obj
 
-    @staticmethod
-    def _get_possible_file_extensions():
-        possible_extensions = ['bytes']
-        possible_extensions.extend(['bytes.' + co.value for co in CompressionToolOption])
-        return possible_extensions
+    def delete_version(self, dt: datetime):
+        with self.load_guard(), self._repository_lock:
+            self._delete_savefile(dt)
 
-    def _extract_datetime_from_filename(self, filename: str) -> datetime | None:
-        obj_cls_name = self._cls.generate_hyphen_separated_name()
-        pattern = (r'^(\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2})_' + obj_cls_name +
-                   r'\.(' + "|".join(self._get_possible_file_extensions()) + r')$')
-        match = re.match(pattern, filename)
-        if match:
-            datetime_str = match.group(1)
-            try:
-                datetime_obj = datetime.strptime(datetime_str, self._dt_format)
-                return datetime_obj
-            except ValueError:
-                logging.error(f"[{self._cls.__name__}] invalid datetime format in filename: {filename}")
-                return None
-        else:
-            logging.warning(f"[{self._cls.__name__}] file that doesn't match format {self._dt_format}_{obj_cls_name} "
-                            f"was found: {filename}")
-        return None
+    def delete_all_versions(self):
+        with self.load_guard(), self._repository_lock:
+            self._delete_all_savefiles()
 
-    def _generate_filename_from_datetime(self, dt: datetime) -> str:
-        dt_str = dt.strftime(self.__class__._dt_format)
-        return f"{dt_str}_{self._cls.generate_hyphen_separated_name()}.bytes"
+    def _write_savefile(self, obj: PersistableObject, dt: datetime | None = None):
+        if dt is None:
+            dt = datetime.now()
+        savefile_path = self._config.resolved_directory / self._get_savefile_name(dt)
+        obj.save_to_file(filepath=savefile_path)
+        if self._config.compression and self._config.compression.enable:
+            compression_tool = self._config.compression.resolved_tool
+            compression_tool.compress(filepath=savefile_path)
+            savefile_path.unlink()
 
-    def _list_sorted_persisted_filepaths_unsafe(self) -> list[Path]:
-        dt_filenames = []
+    def _read_savefile(self, dt: datetime | None = None) -> PersistableObject:
+        savefile_paths = self._get_savefiles_dict()
+        if not savefile_paths:
+            raise FileNotFoundError()
+        if dt is None:
+            # read most recent saved file, if no date is given
+            dt = max(savefile_paths.keys())
+        savefile_path = savefile_paths.get(dt)
+        if savefile_path is None:
+            raise FileNotFoundError()
+        if self._is_compressed_savefile(savefile_path):
+            compression_tool = CompressionToolOption.get_tool_implementation(savefile_path.suffix[1:])
+            extracted_savefile = Path(savefile_path.parent) / savefile_path.stem
+            compression_tool.decompress(savefile_path, output_filepath=extracted_savefile)
+            obj = self._cls.load_from_file(extracted_savefile)
+            extracted_savefile.unlink()
+            return obj
+        return self._cls.load_from_file(savefile_path)
+
+    def _get_savefile_name(self, dt: datetime):
+        return f"{dt.strftime(self._dt_format)}_{self._suffix}"
+
+    def _delete_savefile(self, dt: datetime):
+        path = self._config.resolved_directory / self._get_savefile_name(dt)
+        if not self._is_savefile(path):
+            raise FileNotFoundError()
+        path.unlink()
+
+    def _delete_all_savefiles(self):
+        for path in self._iter_savefiles():
+            path.unlink()
+
+    def _is_savefile(self, path: Path):
+        if not path.exists() or not path.is_file():
+            return False
+        if path.name.endswith(self._suffix):
+            return True
+        if self._is_compressed_savefile(path):
+            return True
+        return False
+
+    def _is_compressed_savefile(self, path: Path):
+        for compression_tool_option in CompressionToolOption:
+            cto_suffix = '.' + compression_tool_option.value
+            if path.name.endswith(self._suffix + cto_suffix):
+                return True
+        return False
+
+    def _iter_savefiles(self) -> Iterable[Path]:
         for path in self._config.resolved_directory.iterdir():
-            if not path.is_file():
+            if not self._is_savefile(path):
                 continue
-            dt_file = self._extract_datetime_from_filename(filename=path.name)
-            if dt_file is None:
-                continue
-            dt_filenames.append((dt_file, path))
-        dt_filenames.sort(key=lambda t: t[0])
-        return [p for dt, p in dt_filenames]
+            yield path
 
-    def list_sorted_persisted_filepaths(self) -> list[Path]:
-        with self._persisted_files_lock:
-            return self._list_sorted_persisted_filepaths_unsafe()
+    def _get_savefiles_dict(self) -> dict[datetime, Path]:
+        saved_obj_paths = dict()
+        for path in self._iter_savefiles():
+            dt_str, _ = path.name.split('_')
+            dt = datetime.strptime(dt_str, self._dt_format)
+            saved_obj_paths[dt] = path
+        return saved_obj_paths
 
-    def _delete_oldest_files_if_max_is_reached(self):
-        sorted_filepaths = self.list_sorted_persisted_filepaths()
-        num_files = len(sorted_filepaths)
-        max_files = self._config.max_saved_models
-        if num_files < max_files:
-            return
-        num_files_to_delete = num_files - max_files + 1
-        logging.info(f'[{self._cls.__name__}] initiating old file purge.')
-        with self._persisted_files_lock:
-            for filepath in sorted_filepaths[:num_files_to_delete]:
-                logging.info(f'[{self._cls.__name__}] deleting file: {filepath}')
-                filepath.unlink()
-
-    def _compress_files_and_delete_unsafe(self):
-        compression_tool = CompressionToolOption.get_tool_implementation(self._config.compression.tool)
-        sorted_filepaths = self._list_sorted_persisted_filepaths_unsafe()
-        if not self._config.compression.compress_latest_model:
-            sorted_filepaths = sorted_filepaths[:-1]
-        for filepath in sorted_filepaths:
-            if not str(filepath).endswith('bytes'):
-                continue
-            compression_tool.compress(filepath)
-            filepath.unlink()
-
-    def compress_files_and_delete(self):
-        with self._persisted_files_lock:
-            self._compress_files_and_delete_unsafe()
-
-    def persist_current_object(self):
-        if not self._config.enable:
-            return
-        self._delete_oldest_files_if_max_is_reached()
-        filename = self._generate_filename_from_datetime(dt=datetime.now())
-        self._obj.save_to_file(filepath=self._config.resolved_directory / filename)
-        if self._config.compression.enable:
-            self.compress_files_and_delete()
-
-    def _uncompress_file_if_compressed_and_get_filepath(self, filepath: Path) -> Path | None:
-        if not any(str(filepath).endswith(ext) for ext in self._get_possible_file_extensions()):
-            logging.error(f'[{self._cls.__name__}] file with unidentified extension: {filepath}')
-            return None
-        if str(filepath).endswith('bytes'):
-            return filepath
-        file_extension = str(filepath).split('.')[-1]
-        compression_tool = CompressionToolOption.get_tool_implementation(file_extension)
-        extracted_path = Path('.'.join(str(filepath).split('.')[:-1]))
-        with self._persisted_files_lock:
-            compression_tool.decompress(compressed_filepath=filepath, output_filepath=extracted_path)
-            filepath.unlink()
-        return extracted_path
-
-    def load_object(self, obj: PersistableObject):
-        if not isinstance(obj, self._cls):
-            logging.error(f'[{self._cls.__name__}] tried to load object of invalid type: {obj.__class__.__name__}')
-            return
-        with self._obj_lock:
-            self._obj = obj
-
-    def load_object_from_file(self, filepath: Path):
-        filepath = self._uncompress_file_if_compressed_and_get_filepath(filepath=filepath)
-        new_obj = self._cls.load_from_file(filepath) if filepath is not None else None
-        if new_obj is None:
-            logging.error(f'[{self.__class__.__name__}] unable to load latest file: {filepath}')
-            return
-        self.load_object(obj=new_obj)
-        logging.info(f'[{self.__class__.__name__}] file loaded successfully: {filepath}')
-
-    def load_object_from_latest_file(self):
-        sorted_filepaths = self.list_sorted_persisted_filepaths()
-        latest_file_path = sorted_filepaths[-1]
-        logging.info(f'[{self._cls.__name__}] started to load latest file: {latest_file_path}')
-        self.load_object_from_file(filepath=latest_file_path)
+    @property
+    def empty_repository(self) -> bool:
+        with self._repository_lock:
+            for _ in self._iter_savefiles():
+                return False
+        return True
--- src/tests/persistence.py
+++ src/tests/persistence.py
@@ -1,11 +1,11 @@
 import pickle
 
 from src.common.config import PersistenceConfig, ConfigurationManager
-from src.services.persistence import PersistableObjectLifeCycleManager
+from src.services.persistence import VersionedObjectManager
 from src.common.persistence import PersistableObject
 
 
-class TestClass(PersistableObject):
+class TClass(PersistableObject):
     def __init__(self, n: int = 0):
         super().__init__()
         self.n = n
@@ -18,25 +18,27 @@ class TestClass(PersistableObject):
         self.n = n
 
 
-class TestClassLCManager(PersistableObjectLifeCycleManager[TestClass]):
+class TClassVersionManager(VersionedObjectManager[TClass]):
     def __init__(self, persistence_config: PersistenceConfig = None):
-        super().__init__(persistable_object_cls=TestClass, persistence_config=persistence_config)
+        super().__init__(cls=TClass)
 
     def get_number(self) -> int:
-        return self.get_object().n
+        return self.get_loaded_version().n
 
 
 if __name__ == '__main__':
-    tcm = TestClassLCManager(persistence_config=ConfigurationManager().get_server_config().ai_module.persistence)
+    ConfigurationManager().load()
+    tcm = TClassVersionManager(persistence_config=ConfigurationManager().get_server_config().ai_module.persistence)
 
     # test create, load and persist object
-    tc = TestClass(n=3)
-    tcm.load_object(obj=tc)
-    tcm.persist_current_object()
+    tc = TClass(n=3)
+    tcm.load_new_version(obj=tc)
+    tc1 = tcm.get_loaded_version()
+    print(tc1.n)
 
     # test load object from latest saved file
-    # tcm.load_object_from_latest_file()
-    # print(tcm.get_object().n)
+    tcm.load_latest_version()
+    print(tcm.get_loaded_version().n)
 
     # test managers are singletons
     # tc = TestClass(n=5)
