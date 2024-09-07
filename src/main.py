f8d04395ba4a6f393645c31b9d883745c6efa805
Bruno Burin <cburin.bruno@gmail.com>
2024-09-07 19:08:25 -0300

    [back] add retraining periodic task

--- src/main.py
+++ src/main.py
@@ -1,29 +1,41 @@
 import uvicorn
 
+from src.ai_module.ensamble_manager import EnsembleManager
+from src.ai_module.pipeline import train_pipeline
 from src.api.api import api
-from src.common.config import ConfigurationManager
+from src.common.config import ConfigurationManager, ServerConfig
 from src.models.user import User
 from src.services.auth import TokenAuthManager
 from src.services.database import DBSessionManager
-from src.services.task import TaskManager
+from src.services.task import TaskManager, PeriodicTask
 
 
-def build_task_manager() -> TaskManager:
+def build_task_manager(config: ServerConfig) -> TaskManager:
     tm = TaskManager()
-    tm.add_startup_task(ConfigurationManager().load, name="LoadConfigs")\
-      .add_startup_task(DBSessionManager().load, name="LoadDBSessionManager")\
+    # create periodic tasks
+    retraining_task = PeriodicTask(
+        cron_string=config.ai_module.training.cron_string,
+        task=train_pipeline
+    )
+    # add tasks
+    tm.add_startup_task(DBSessionManager().load, name="LoadDBSessionManager")\
       .add_startup_task(DBSessionManager().create_db_and_tables, name="CreateDBAndTables")\
       .add_asynchronous_task(User.create_admin_if_none_exists, name="CreateAdminIfNoneExists",
                              session=DBSessionManager().get_session())\
-      .add_asynchronous_task(TokenAuthManager().load, name="LoadTokenAuthManager")
+      .add_asynchronous_task(TokenAuthManager().load, name="LoadTokenAuthManager")\
+      .add_asynchronous_task(EnsembleManager().load, name="LoadEnsembleManager")\
+      .add_periodic_task(retraining_task, name="ModelRetrainingTask")
     return tm
 
 
 def main():
-    tm = build_task_manager()
+    # load configs
+    ConfigurationManager().load()
+    config = ConfigurationManager().get_server_config()
+    # run tasks at startup
+    tm = build_task_manager(config)
     tm.run_all_tasks()
     # run server
-    config = ConfigurationManager().get_server_config()
     uvicorn.run("src.main:api", host=config.host, port=config.port, reload=config.dev_mode)
 
 
